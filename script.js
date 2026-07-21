6421bd777385a0ee10afb35dd17c9abfbd78b636
Ashwini Lavanya J <ashwinilavanya2001@gmail.com>
2026-07-21 17:27:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,3 +83,22 @@ const navMenu = document.querySelector("nav ul");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
